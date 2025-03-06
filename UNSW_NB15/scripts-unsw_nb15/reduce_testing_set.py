29fb39f62eb928c5ce2876617be10a49297c2f93
Ifra <ifraaabdulrauf@gmail.com>
2025-03-07 00:29:43 +0500

    Added script to reduce testing set to 50k rows

---
+++ UNSW_NB15/scripts-unsw_nb15/reduce_testing_set.py
@@ -0,0 +1,13 @@
+import polars as pl
+
+# Load dataset from CSV
+df = pl.read_csv("UNSW_NB15/data/UNSW_NB15_testing-set.csv")
+
+# Randomly select 50,000 rows
+sampled_df = df.sample(n=50000, shuffle=True, seed=42)  # Shuffle ensures randomness
+
+# Save as CSV
+csv_path = "UNSW_NB15/cleaned_data/reduced_testing_set.csv"
+sampled_df.write_csv(csv_path)
+
+print(f"Saved 50,000 random rows to {csv_path}")
