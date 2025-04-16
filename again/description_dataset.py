e300617d0f4227b8a61aaacbd997552f4c8230a2
Mulaimboy <bscs23059@itu.edu.pk>
2025-04-17 01:35:26 +0500

    dealt with inf and -inf and description of dataset

---
+++ again/description_dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+pd.set_option('display.max_rows', None, 'display.max_columns', None)
+# Load your dataset
+input_path = r"D:\4th semester\SE\project\Dataset\removed_neg_inf.csv" # Replace with your actual path
+df = pd.read_csv(input_path)
+
+# Display the first few rows
+print("📄 Preview of the dataset:\n")
+print(df.head())
+
+# Display dataset info
+print("\nℹ️ Dataset info:\n")
+print(df.info())
