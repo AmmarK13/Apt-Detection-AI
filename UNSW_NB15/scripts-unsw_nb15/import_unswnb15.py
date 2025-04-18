c44878553702d86a93c0a89a24d0aeb0c09608f6
Ifra <ifraaabdulrauf@gmail.com>
2025-04-18 15:48:41 +0500

    reorganised structure

--- UNSW_NB15/scripts-unsw_nb15/import_unswnb15.py
+++
@@ -1,5 +0,0 @@
-import kaggle
-
-# Download UNSW-NB15 dataset from Kaggle
-kaggle.api.dataset_download_files('dhoogla/unswnb15', path='data/', unzip=True)
-print("Dataset downloaded to 'data/' successfully")
